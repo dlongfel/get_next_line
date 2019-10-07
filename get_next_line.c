718a74678694b00cb6a90e210cfff2b6c5f5586f
Dolores Longfellow <dlongfel@mi-i5.21-school.ru>
2019-10-07 19:39:49 +0300

    sch

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,55 @@
 /*   By: dlongfel <dlongfel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/21 17:14:23 by dlongfel          #+#    #+#             */
-/*   Updated: 2019/10/05 14:14:22 by dlongfel         ###   ########.fr       */
+/*   Updated: 2019/10/07 19:37:41 by dlongfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(int const fd, char **line)
+int	get_line(const int fd, char **line, char *rest)
 {
-	char	buf[10+1];
-	int		was_read;
+	char	buf[BUFF_SIZE + 1];
+	char	*p_n;
+	char	*tmp;
+	int		rd;
 
-	while ((was_read = read = (fd, buf, 10)))
+	p_n = NULL;
+	rd = 1;
+	*line = checkrest(&p_n, rest);
+	while (p_n == 0 && ((rd = read(fd, buf, BUFF_SIZE)) != 0))
 	{
-		buf[was_read] = '\0';
-		*line = ft_strjoin(*line, buf);
+		buf[rd] = '\0';
+		if ((p_n == ft_strchr(buf, '\n')) != NULL)
+		{
+			ft_strcpy(rest, ++p_n);
+			ft_strclr(--p_n);
+		}
+		tmp = *line;
+		if (!(*line = ft_strchr(buf, '\n')) != NULL)
+			return (-1);
+		ft_strdel(&tmp);
 	}
-	return (0);
+	return ((ft_strlen(*len) || ft_strlen(rest) || rd) ? 1: 0);
 }
 
-int		main(void)
+int		get_next_line(int const fd, char **line)
 {
-	char	*line;
-	int		fd;
+	static t_arr	*list;
+	t_arr			*tmp;
+	int				ret;
 
-	line = "hello world";
-	fd = open(text.txt, O_RDONLY);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
+	if (fd < 0 || line == 0)
+		return (-1);
+	if (!list)
+		list = newlist(fd);
+	tmp = list;
+	while (tmp->fd != fd)
+	{
+		if (tmp->next == NULL)
+			tmp->next = newlist(fd);
+		tmp = tmp->rest;
+	}
+	ret = get_line(fd, line, tmp->rest);
+	return (ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dlongfel <dlongfel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/21 17:13:56 by dlongfel          #+#    #+#             */
-/*   Updated: 2019/09/23 10:06:54 by dlongfel         ###   ########.fr       */
+/*   Updated: 2019/10/07 19:37:30 by dlongfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,21 @@
 # include <sys/types.h>
 # include <unistd.h>
 # include <stdlib.h>
+#include <stdio.h>
 # include "libft/libft.h"
 
 # define BUFF_SIZE 7
 
+typedef struct		s_arr
+{
+	int				fd;
+	char			*rest;
+	struct s_arr	*next;
+}					t_arr;
+
+t_arr				*ft_newlist(const int fd);
+char				*checkrest(char **p_n, char *rest);
+int					get_line(const int fd, char **line, char *rest);
 int		get_next_line(int const fd, char **line);
 
 #endif
